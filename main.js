9f8bf461467cff70a6f14c1d2f0cdea8d9574b05
Evgeniy37529 <lognard1994@gmail.com>
2021-09-20 20:05:56 +0300

    Add menu-burger & adaptive

---
+++ main.js
@@ -0,0 +1,6 @@
+const buttonMenu = document.querySelector('.button-menu');
+const nav = document.querySelector('.nav');
+buttonMenu.addEventListener('click' , function(){
+    nav.classList.toggle('show-nav');
+});
+console.log('Score 150');
